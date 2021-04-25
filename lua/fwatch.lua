f514a65b7569e50f1fc8260fa45fceb94dbc6c14
Oliver Marriott <hello@omarriott.com>
2021-04-25 16:24:17 +1000

    Make work gooder

--- lua/fwatch.lua
+++ lua/fwatch.lua
@@ -1,73 +1,101 @@
 local uv = vim.loop
--- 
-local watchers = {}
-local M = {}
 
--- watches path and calls on_event(filename, events) or on_error(error)
-local function watch_with_function(path, on_event, on_error)
-  -- check for 'fail', what is 'fail'?
+-- @doc """
+-- Watch path and calls on_event(filename, events) or on_error(error)
+--
+-- opts:
+--  is_oneshot -> don't reattach after running, no matter the return value
+-- """
+local function watch_with_function(path, on_event, on_error, opts)
+  -- TODO: check for 'fail', what is 'fail'?
   local handle = uv.new_fs_event()
 
-  -- these are all default
+  -- these are just the default values
   local flags = {
-    watch_entry = false,
-    stat = false,
-    recursive = false
+    watch_entry = false, -- true = when dir, watch dir inode, not dir content
+    stat = false, -- true = don't use inotify/kqueue but periodic check, not implemented
+    recursive = false -- true = watch dirs inside dirs
   }
 
-  -- attach handler
-  uv.fs_event_start(handle, path, flags, function(err, filename, events)
+  local callback = function(err, filename, events)
+    local remain_attached
     if err then
-      local remain_attached = on_error(error)
-      if not remain_attached then
-        uv.fs_event_stop(handle)
-      end
+      remain_attached = on_error(error)
     else
-      local remain_attached = on_event(filename, events)
-      if not remain_attached then
-        uv.fs_event_stop(handle)
-      end
+      remain_attached = on_event(filename, events)
     end
-  end)
+    if opts.is_oneshot or remain_attached == false then
+      uv.fs_event_stop(handle)
+    end
+  end
+
+  -- attach handler
+  uv.fs_event_start(handle, path, flags, callback)
 
   return handle
 end
 
-local function watch_with_string(path, string)
+-- @doc """
+-- Watch a path and run given string as an ex command
+--
+-- Internally creates on_event and on_error handler and
+-- delegates to watch_with_function.
+--
+-- """
+local function watch_with_string(path, string, opts)
+  -- just run the command and reattach
   local on_event = function(_, _)
     vim.schedule(function()
       vim.cmd(string)
     end)
+
+    -- may not re-attach if opts.is_oneshot = true
+    return true
   end
+
+  -- log the error and reattach
   local on_error = function(error)
-    print("fwatch with with command: " .. string .. " encounterd an error: " .. error)
+    error("fwatch with with command: " .. string .. " encounterd an error: " .. error)
+
+    -- always re-attach error so it's noticed
+    -- may not re-attach if opts.is_oneshot = true
+    return true
   end
-  return watch_with_function(path, on_event, on_error)
+
+  -- delegate out
+  return watch_with_function(path, on_event, on_error, opts)
 end
 
--- creates a new watcher
-local function watch(path, runnable)
+-- @doc """
+-- Sniff parameters and call appropriate watch handler
+-- """
+local function do_watch(path, runnable, opts)
   if type(runnable) == "string" then
-    return watch_with_string(path, runnable)
+    return watch_with_string(path, runnable, opts)
   elseif type(runnable) == "table" then
-    assert(runnable.on_event, "must provide on_event")
-    assert(runnable.on_error, "must provide on_error")
-    return watch_with_function(path, runnable.on_event, runnable.on_error)
+    assert(runnable.on_event, "must provide on_event to watch")
+    assert(runnable.on_error, "must provide on_error to watch")
+    return watch_with_function(path, runnable.on_event, runnable.on_error, opts)
   else
-    error("Unknown runnable type given to watch")
+    error("Unknown runnable type given to watch," ..
+      " must be string or {on_event = function, on_error = function}.")
   end
 end
 
-local function unwatch(handle)
-  uv.fs_event_stop(handle)
-end
-
--- local function once(path, runnable)
---   local handle = watch(path, runnable)
--- end
-
-return {
-  watch = watch,
-  -- once = once,
-  unwatch = unwatch
+M = {
+  watch = function (path, runnable)
+    return do_watch(path, runnable, {
+      is_oneshot = false
+    })
+  end,
+  unwatch = function (handle)
+    return uv.fs_event_stop(handle)
+  end,
+  once = function (path, runnable)
+    return do_watch(path, runnable, {
+      is_oneshot = true
+    })
+  end
 }
+
+return M
